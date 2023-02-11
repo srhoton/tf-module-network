39cb6abe3d0b6b17192df27ff6ce5ffb19966349
Stephen Rhoton <stephen.rhoton@dealerware.com>
2023-02-11 13:32:07 -0500

    Add output variables

---
+++ output.tf
@@ -0,0 +1,12 @@
+output "private_subnet_list" {
+  value = [aws_subnet.default_subnets["private-1"].id, aws_subnet.default_subnets["private-2"].id, aws_subnet.default_subnets["private-3"].id]
+}
+
+output "public_subnet_list" {
+  value = [aws_subnet.default_subnets["public-1"].id, aws_subnet.default_subnets["public-2"].id, aws_subnet.default_subnets["public-3"].id]
+}
+
+output "vpc_id" {
+  value = aws_vpc.default_vpc.id 
+}
+  
