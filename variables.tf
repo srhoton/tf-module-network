24a18f594814cf7c5645c807fc0c994cfd04297d
Stephen Rhoton <stephen.rhoton@dealerware.com>
2023-02-11 07:28:20 -0500

    Add az mappings

--- variables.tf
+++ variables.tf
@@ -2,3 +2,17 @@ variable "base_cidr_block" {
   type = string
   default = "192.168.0.0/16"
 }
+
+variable "az_mapping" {
+  description = "AZ names to their zones"
+  default = {
+    "private-1" = "us-west-2a"
+    "private-2" = "us-west-2b"
+    "private-3" = "us-west-2c"
+    "public-1" = "us-west-2a"
+    "public-2" = "us-west-2b"
+    "public-3" = "us-west-2c"
+
+  }
+}
+
