dddccaf80ce71ea52bc0af919854fcb4c53aac63
Stephen Rhoton <stephen.rhoton@dealerware.com>
2023-08-31 13:31:51 -0400

    Change references

--- client_vpn.tf
+++ client_vpn.tf
@@ -21,11 +21,11 @@ resource "aws_ec2_client_vpn_endpoint" "client_vpn_endpoint" {
   description = "Client Vpn Endpoint for ${var.env_name}"
   client_cidr_block = module.subnet_addrs.network_cidr_blocks["vpn-1"]
   split_tunnel = true
-  server_certificate_arn = data.aws_acm_certificate.server_vpn_root.arn
+  server_certificate_arn = data.aws_acm_certificate.server_vpn_root[0].arn
 
   authentication_options {
     type = "certificate-authentication"
-    root_certificate_chain_arn = data.aws_acm_certificate.client_vpn_root.arn
+    root_certificate_chain_arn = data.aws_acm_certificate.client_vpn_root[0].arn
   }
   
   connection_log_options {
